fe96487374a900076db7ecba9f6fdf1e8ec377cb
brighton hoods <brightonmajiwa@gmail.com>
2022-01-22 11:13:41 +0300

    fixed issues with apptest file code

--- src/App.test.js
+++ src/App.test.js
@@ -10,6 +10,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   const element = render( < App / > );
-  const linkElement = el.getByText(/learn react/i);
+  const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
